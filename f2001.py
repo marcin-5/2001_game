f4922100fa25e76264e090015a26d0c103417e17
marcin-5 <marcin_5@mail.ru>
2022-12-19 17:39:46 +0100

    game logic in separate function

--- f2001.py
+++ f2001.py
@@ -5,17 +5,20 @@ from c2001 import calculate_points
 from random import randrange
 
 app = Flask(__name__)
+form_names = ("upts", "cpts", "ud1", "ud2", "cd1", "cd2",
+              "win", "ur1", "ur2", "cr1", "cr2", "rk")
 
 
-@app.route("/2001", methods=["GET", "POST"])
-def f2001():
-    if request.method == "POST":
-        user_points = int(request.form["upts"])
-        computer_points = int(request.form["cpts"])
-        user_dice_1 = request.form["ud1"]
-        user_dice_2 = request.form["ud2"]
-        computer_dice_1 = POSSIBLE_DICES[randrange(0, len(POSSIBLE_DICES))]
-        computer_dice_2 = POSSIBLE_DICES[randrange(0, len(POSSIBLE_DICES))]
+def parse_turn(user_points=0, computer_points=0,
+               user_dice_1="", user_dice_2=""):
+    computer_dice_1 = POSSIBLE_DICES[randrange(0, len(POSSIBLE_DICES))]
+    computer_dice_2 = POSSIBLE_DICES[randrange(0, len(POSSIBLE_DICES))]
+    if not user_dice_1 and not user_dice_2:
+        # start the game with default values
+        user_roll_1 = user_roll_2 = 0
+        computer_roll_1 = computer_roll_2 = 0
+        user_points = computer_points = 0
+    else:
         user_roll_1 = roll_the_dice(user_dice_1)
         user_roll_2 = roll_the_dice(user_dice_2)
         user_roll_sum = user_roll_1 + user_roll_2
@@ -24,23 +27,34 @@ def f2001():
         computer_roll_sum = computer_roll_1 + computer_roll_2
         user_points = calculate_points(user_roll_sum, user_points)
         computer_points = calculate_points(computer_roll_sum, computer_points)
-        if user_points > 2001 or computer_points > 2001:
-            if user_points > computer_points: win = 1
-            elif user_points < computer_points: win = 2
-            else: win = 3
-        else: win = 0
-        return render_template("2001.html",
-                               rk=POSSIBLE_DICES, win=win,
-                               upts=user_points, cpts=computer_points,
-                               ud1=user_dice_1, ud2=user_dice_2,
-                               cd1=computer_dice_1, cd2=computer_dice_2,
-                               ur1=user_roll_1, ur2=user_roll_2,
-                               cr1=computer_roll_1, cr2=computer_roll_2)
+    if user_points > 2001 or computer_points > 2001:
+        if user_points > computer_points:
+            win = 1
+        elif user_points < computer_points:
+            win = 2
+        else:
+            win = 3
     else:
+        win = 0
+    return dict(zip(form_names,
+                    (user_points, computer_points,
+                     user_dice_1, user_dice_2,
+                     computer_dice_1, computer_dice_2,
+                     win, user_roll_1, user_roll_2,
+                     computer_roll_1, computer_roll_2,
+                     POSSIBLE_DICES)))
+
+
+@app.route("/2001", methods=["GET", "POST"])
+def f2001():
+    if request.method == "POST":
         return render_template("2001.html",
-                               rk=POSSIBLE_DICES, win=0,
-                               upts=0, cpts=0,
-                               cd1="D3", cd2="D3")
+                               **parse_turn(int(request.form["upts"]),
+                                            int(request.form["cpts"]),
+                                            request.form["ud1"],
+                                            request.form["ud2"]))
+    else:
+        return render_template("2001.html", **parse_turn())
 
 
 if __name__ == "__main__":
