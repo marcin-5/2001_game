7512fe0645786a28b19edac3efd817aea7b97fab
marcin-5 <marcin_5@mail.ru>
2022-12-21 18:33:23 +0100

    use adapted to the game rolling dices function

--- c2001.py
+++ c2001.py
@@ -1,6 +1,28 @@
-from dice import roll_the_dice_re as roll_the_dice
-from dice import POSSIBLE_DICES
-from random import randrange
+from random import randint, randrange
+
+
+def roll_the_dices(number_of_dices, rnd=False):
+    """
+    Simulates roll the dices (random or from input)
+    :param int number_of_dices:
+    :param boolean rnd:
+    :return: Two tuples: codes of used dices and results
+    """
+    possible_dices = tuple(f"D{_}" for _ in (3, 4, 6, 8, 10, 12, 20, 100))
+    results = []
+    for i in range(number_of_dices):
+        if not i:
+            print(f"Choose dice{'s' if number_of_dices > 1 else ''} from: {', '.join(possible_dices)}.")
+        if rnd:
+            code = possible_dices[randrange(len(possible_dices))]
+        else:
+            code = ""
+            while not code or code not in possible_dices:
+                code = input(f"Dice {i+1}: ")
+                if code not in possible_dices:
+                    print(f"Wrong input for dice {i+1}!")
+        results.append((code, randint(1, int(code[1:]))))
+    return tuple(_[0] for _ in results), tuple(_[1] for _ in results)
 
 
 def calculate_points(roll, points):
@@ -24,28 +46,15 @@ def game_2001():
     """2001 game (console version)"""
     user_points = computer_points = 0
     while user_points < 2001 and computer_points < 2001:
-        print(f"Choose dices from: {', '.join(POSSIBLE_DICES)}.")
-        dice1 = dice2 = None
-        while not dice1 or dice1 not in POSSIBLE_DICES:
-            dice1 = input("Dice 1: ")
-            if dice1 not in POSSIBLE_DICES:
-                print("Wrong input for dice 1:", dice1)
-        while not dice2 or dice2 not in POSSIBLE_DICES:
-            dice2 = input("Dice 2: ")
-            if dice2 not in POSSIBLE_DICES:
-                print("Wrong input for dice 2:", dice2)
-        user_roll_1 = roll_the_dice(dice1)
-        user_roll_2 = roll_the_dice(dice2)
-        user_roll_sum = user_roll_1 + user_roll_2
-        user_points = calculate_points(user_roll_sum, user_points)
-        dice1 = POSSIBLE_DICES[randrange(0, len(POSSIBLE_DICES))]
-        dice2 = POSSIBLE_DICES[randrange(0, len(POSSIBLE_DICES))]
-        computer_roll_1 = roll_the_dice(dice1)
-        computer_roll_2 = roll_the_dice(dice2)
-        computer_roll_sum = computer_roll_1 + computer_roll_2
-        computer_points = calculate_points(computer_roll_sum, computer_points)
-        print(f"User roll: {user_roll_1} and {user_roll_2}")
-        print(f"Computer roll: {computer_roll_1} and {computer_roll_2}. Used dices {dice1} and {dice2}")
+        user_rolls = roll_the_dices(2)[1]
+        user_rolls_sum = sum(user_rolls)
+        user_points = calculate_points(user_rolls_sum, user_points)
+        computer_dices, computer_rolls = roll_the_dices(2, rnd=True)
+        computer_rolls_sum = sum(computer_rolls)
+        computer_points = calculate_points(computer_rolls_sum, computer_points)
+        print(user_rolls)
+        print("User roll:", ", ".join(map(str, user_rolls)))
+        print(f"Computer roll:", ", ".join(map(str, computer_rolls)), "Used dices:", ", ".join(computer_dices))
         print(f"User points: {user_points}, computer points: {computer_points}")
     if user_points > computer_points:
         print("User win!!!")
